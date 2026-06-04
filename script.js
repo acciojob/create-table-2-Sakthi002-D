911c77ac2ddc8f2bb19cbaaae65133a88e6cc9e1
Sakthi002-D <sakthivel.d002@gmail.com>
2026-06-04 11:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 function createTable() {
-    //Write your code here
-	const rows = parseInt(prompt("Input number of rows"));
-	const column = parseInt(prompt("Input number of columns"));
+    const rows = parseInt(prompt("Input number of rows"));
+    const column = parseInt(prompt("Input number of columns"));
 
-	if (isNaN(rows) || isNaN(column)  || rows <= 0 || column <= 0) {
-		alert("Invalid input");
-		return;
-	}
+    if (isNaN(rows) || isNaN(column) || rows <= 0 || column <= 0) {
+        alert("Invalid input");
+        return;
+    }
 
+    const table = document.getElementById("myTable");
+    table.innerHTML = "";
 
-	const table = document.getElementById("myTable");
-	table.innerHTML ="";
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement("tr");
 
+        for (let j = 0; j < column; j++) {
+            const td = document.createElement("td");
+            td.textContent = `Row-${i} Column-${j}`;
+            tr.appendChild(td);
+        }
 
-	for(let i = 0; j < rows; i++) {
-		const tr = document.createElement("tr");
-
-
-		for(let j = 0; j < column; j++) {
-			const td = document.createElement("td");
-			td.textContent = `Row-${i}  Column-${j}`;
-			tr.appendChild(td);
-		}
-		table.appendChild(tr);
-	}
-  
+        table.appendChild(tr);
+    }
 }
