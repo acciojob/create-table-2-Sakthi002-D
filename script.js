f3e7d475accd4b23ed36936cfd5b2cb0373223df
Sakthi002-D <sakthivel.d002@gmail.com>
2026-06-04 11:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 function createTable() {
     //Write your code here
+	cosnt rows = parseInt(prompt("Input number of rows"));
+	const column = parseInt(prompt("Input number of columns"));
+
+	if (isNan(rows) || isNaN(column)  || rows <= 0 || column <= 0) {
+		alert("Invalid input");
+		return;
+	}
+
+
+	const table = document.getElementById("myTable");
+	table.innerHTML ="";
+
+
+	for(let i=0; j < rows; i++) {
+		const tr = document.createElement("tr");
+
+
+		for(let j = 0; j < column; j++) {
+			const td = document.createElement("td");
+			td.textContent = `Row-${i}  Column-${j}`;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
   
 }
